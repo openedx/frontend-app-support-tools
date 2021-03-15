00785d6dbbcd341a53b8bc2f896d61487a30005d
DawoudSheraz <dawoud.sheraz@gmail.com>
2021-03-15 12:35:28 +0500

    verification details api tests

--- src/users/data/api.test.js
+++ src/users/data/api.test.js
@@ -6,8 +6,18 @@ import enrollmentsData from './test/enrollments';
 import * as api from './api';
 
 describe('API', () => {
+  const testEmail = 'email@example.com';
   const testUsername = 'username';
   let mockAdapter;
+
+  const throwError = (errorCode, dataString) => {
+    const error = new Error();
+    error.customAttributes = {
+      httpErrorStatus: errorCode,
+      httpErrorResponseData: JSON.stringify(dataString),
+    };
+    throw error;
+  };
 
   beforeEach(() => {
     mockAdapter = new MockAdapter(getAuthenticatedHttpClient());
@@ -59,6 +69,45 @@ describe('API', () => {
 
       const response = await api.getEnrollments(testUsername);
       expect(response).toEqual(expectedData);
+    });
+  });
+
+  describe('User Password Status Fetch', () => {
+    it('Password Status Data', async () => {
+      const apiUrl = `${getConfig().LMS_BASE_URL}/support/manage_user/${testUsername}`;
+      const expectedData = {
+        username: testUsername,
+        date_joined: Date().toLocaleString(),
+        is_active: true,
+        status: 'Usable',
+        password_toggle_history: [],
+        email: testEmail,
+      };
+      mockAdapter.onGet(apiUrl).reply(200, expectedData);
+
+      const response = await api.getUserPasswordStatus(testUsername);
+      expect(response).toEqual(expectedData);
+    });
+  });
+
+  describe('User Verification Details Fetch', () => {
+    const defaultResponse = {
+      sso_verification: [],
+      ss_photo_verification: [],
+      manual_verification: [],
+    };
+    const verificationApiUrl = `${getConfig().LMS_BASE_URL}/api/user/v1/accounts/${testUsername}/verifications/`;
+
+    it('default response is returned if error is raised', async () => {
+      mockAdapter.onGet(verificationApiUrl).reply(() => throwError(500, ''));
+      const response = await api.getUserVerificationDetail(testUsername);
+      expect(response).toEqual(defaultResponse);
+    });
+
+    it('default response is returned if 404 is raised', async () => {
+      mockAdapter.onGet(verificationApiUrl).reply(() => throwError(404, ''));
+      const response = await api.getUserVerificationDetail(testUsername);
+      expect(response).toEqual(defaultResponse);
     });
   });
 });
