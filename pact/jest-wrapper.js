7b2b5b19d8238150d7f76b29d66920d129ac6a94
Azan Bin Zahid <azan.zahid@arbisoft.com>
2021-03-18 21:40:58 +0500

    pact config

---
+++ pact/jest-wrapper.js
@@ -0,0 +1,8 @@
+// ./pact/jest-wrapper.js
+beforeAll((done) => {
+    global.provider.setup().then(() => done());
+});
+
+afterAll((done) => {
+    global.provider.finalize().then(() => done());
+});
