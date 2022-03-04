2a9029cb24f51be3dbddee409b84abf6ce3d835a
Simon Chen <schen@edx.org>
2022-03-04 16:43:15 -0500

    fix: Add tooltip popover for details on IDV attempts history (#230)
    Co-authored-by: Simon Chen <schen@edX-C02FW0GUML85.local>

--- src/users/data/urls.js
+++ src/users/data/urls.js
@@ -47,6 +47,10 @@ export const getVerifiedNameHistoryUrl = username => `${
   LMS_BASE_URL
 }/api/edx_name_affirmation/v1/verified_name/history?username=${username}`;
 
+export const getVerificationAttemptDetailsByIdUrl = attemptId => `${
+  LMS_BASE_URL
+}/api/user/v1/accounts/verifications/${attemptId}/`;
+
 export const getUserPasswordStatusUrl = userIdentifier => `${
   LMS_BASE_URL
 }/support/manage_user/${userIdentifier}`;
