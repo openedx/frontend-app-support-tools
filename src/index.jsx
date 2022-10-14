797c19698a5203deafe793eb62baaf77c759badb
Cristhian Garcia <crisgarta8@gmail.com>
2022-10-14 12:52:55 -0500

    feat: add support for runtime configuration

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import 'babel-polyfill';
 
 import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize, mergeConfig,
+  APP_INIT_ERROR,
+  APP_READY,
+  subscribe,
+  initialize,
+  mergeConfig,
+  getConfig,
 } from '@edx/frontend-platform';
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
-
+import { Helmet } from 'react-helmet';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Switch, Route } from 'react-router-dom';
@@ -34,6 +39,9 @@ subscribe(APP_READY, () => {
     <AppProvider>
       <UserMessagesProvider>
         <Header />
+        <Helmet>
+          <link rel="shortcut icon" href={getConfig().FAVICON_URL} type="image/x-icon" />
+        </Helmet>
         <Switch>
           <Route path="/" component={SupportToolsTab} />
           <Route path="/learner_information" component={UserPage} />
