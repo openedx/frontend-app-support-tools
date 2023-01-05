7078950a929ffe771818e18468ef8a178b857e6f
mashal-m <mashal.malik@arbisoft.com>
2023-01-05 12:40:27 +0500

    refactor: migrate off modal deprecation in password reset history

--- src/users/account-actions/ResetPassword.jsx
+++ src/users/account-actions/ResetPassword.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
-import { Modal, Button, Alert } from '@edx/paragon';
+import {
+  Button, Alert, ModalDialog, ActionRow,
+} from '@edx/paragon';
 import { postResetPassword } from '../data/api';
 
 export default function ResetPassword({
@@ -53,25 +55,40 @@ export default function ResetPassword({
         className="mr-1 mb-2"
       >Reset Password
       </Button>
-
-      <Modal
-        open={resetPasswordModalIsOpen}
-        id="user-account-reset-password"
-        buttons={[errorMessage ? (<></>)
-          : (
-            <Button
-              variant="danger"
-              onClick={resetPassword}
-            >
-              Confirm
-            </Button>
-          ),
-        ]}
+      <ModalDialog
+        isOpen={resetPasswordModalIsOpen}
         onClose={closeResetPasswordModal}
-        dialogClassName="modal-lg modal-dialog-centered justify-content-center"
-        title="Reset Password"
-        body={modalBody}
-      />
+        hasCloseButton
+        id="user-account-reset-password"
+        size="lg"
+      >
+        <ModalDialog.Header className="mb-3 mt-1">
+          <ModalDialog.Title className="modal-title">
+            Reset Password
+          </ModalDialog.Title>
+        </ModalDialog.Header>
+        <ModalDialog.Body className="mb-3">
+          {modalBody}
+        </ModalDialog.Body>
+        <ModalDialog.Footer>
+          <ActionRow>
+            <ModalDialog.CloseButton
+              variant="link"
+            >
+              Close
+            </ModalDialog.CloseButton>
+            {errorMessage ? (<></>)
+              : (
+                <Button
+                  variant="danger"
+                  onClick={resetPassword}
+                >
+                  Confirm
+                </Button>
+              )}
+          </ActionRow>
+        </ModalDialog.Footer>
+      </ModalDialog>
     </div>
   );
 }
