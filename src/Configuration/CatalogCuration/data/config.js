390d32adbaf00baec0c38d762ae687f7fa5cf2df
Hamzah Ullah <hamzahullah@yahoo.com>
2023-04-10 16:44:14 -0400

    feat: catalog-search implemented (#319)

---
+++ src/Configuration/CatalogCuration/data/config.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/prefer-default-export */
+export const configuration = {
+  ALGOLIA: {
+    APP_ID: process.env.ALGOLIA_APP_ID,
+    SEARCH_API_KEY: process.env.ALGOLIA_SEARCH_API_KEY,
+    INDEX_NAME: process.env.ALGOLIA_INDEX_NAME,
+  },
+};
