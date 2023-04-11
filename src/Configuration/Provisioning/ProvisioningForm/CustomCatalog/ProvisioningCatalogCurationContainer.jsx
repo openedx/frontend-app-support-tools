ef2efeb31c7c044f5967654ad55d09a4c58f5b9c
Alexander J Sheehan <asheehan@2u.com>
2023-04-11 20:17:16 +0000

    feat: adding enterprise catalog client

--- src/Configuration/Provisioning/ProvisioningForm/CustomCatalog/ProvisioningCatalogCurationContainer.jsx
+++ src/Configuration/Provisioning/ProvisioningForm/CustomCatalog/ProvisioningCatalogCurationContainer.jsx
@@ -1,10 +1,36 @@
 import CatalogCurationContextProvider from '../CatalogCuration/CatalogCurationContext';
 import CatalogCurationSearch from '../CatalogCuration/CatalogCurationSearch';
+import { Button } from '@edx/paragon';
 
-const ProvisioningCatalogCurationContainer = () => (
-  <CatalogCurationContextProvider>
+import EnterpriseCatalogApiService from '../../../../data/services/EnterpriseCatalogApiService';
+import { selectProvisioningContext } from '../../data/utils';
+import { logError } from '@edx/frontend-platform/logging';
+
+
+const ProvisioningCatalogCurationContainer = () => {
+  const [formData] = selectProvisioningContext('formData');
+  const handleClick = () => {
+    EnterpriseCatalogApiService.generateNewCustomerCatalog(
+      formData.enterpriseUUID,
+      { 'data': 'foobar' },
+    )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log("sad");
+      });
+    console.log("wassup");
+  };
+
+  return (<CatalogCurationContextProvider>
     <CatalogCurationSearch />
-  </CatalogCurationContextProvider>
-);
+    <Button
+      onClick={handleClick}
+    >
+      Submit Catalog
+    </Button>
+  </CatalogCurationContextProvider>)
+};
 
 export default ProvisioningCatalogCurationContainer;
