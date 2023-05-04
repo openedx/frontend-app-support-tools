ab6cd1b00413cafd886d291798b75730c2f13611
Hamzah Ullah <hamzahullah@yahoo.com>
2023-05-04 12:24:45 -0400

    feat: removed redundant form field

--- src/Configuration/Provisioning/ProvisioningForm/CustomCatalog/ProvisioningFormCustomCatalog.jsx
+++ src/Configuration/Provisioning/ProvisioningForm/CustomCatalog/ProvisioningFormCustomCatalog.jsx
@@ -3,41 +3,27 @@ import ProvisioningFormDefineCustomCatalogHeader from './ProvisioningFormDefineC
 import ProvisioningFormCustomCatalogDropdown from './ProvisioningFormCustomCatalogDropdown';
 import ProvisioningFormCustomCatalogTitle from './ProvisioningFormCustomCatalogTitle';
 import ProvisioningFormCustomCatalogTextArea from './ProvisioningFormCustomCatalogTextArea';
-import ProvisioningFormEnterpriseCustomerCatalog from './ProvisioningFormEnterpriseCustomerCatalog';
 import { indexOnlyPropType, selectProvisioningContext } from '../../data/utils';
 import ProvisioningFormCustomCatalogExecEdBoolean from './ProvisioningFormCustomCatalogExecEdBoolean';
 
 const ProvisioningFormCustomCatalog = ({ index }) => {
   const [formData] = selectProvisioningContext('formData');
   const [policyData, setPolicyData] = useState(formData.policies[0].catalogQueryMetadata);
-  const [customerCatalogBoolean, setCustomerCatalogBoolean] = useState(formData.policies[0].customerCatalog);
 
   useEffect(() => {
     setPolicyData(formData.policies[index].catalogQueryMetadata);
-    setCustomerCatalogBoolean(formData.policies[index].customerCatalog);
   }, [formData.policies[index].catalogQueryMetadata, formData.policies[index].customerCatalog]);
 
-  if (customerCatalogBoolean) {
-    return (
-      <article className="mt-4.5">
-        <ProvisioningFormDefineCustomCatalogHeader index={index} />
-        <ProvisioningFormEnterpriseCustomerCatalog index={index} />
-      </article>
-    );
-  }
-  if (customerCatalogBoolean === false) {
-    return (
-      <article className="mt-4.5">
-        <ProvisioningFormDefineCustomCatalogHeader index={index} />
-        <ProvisioningFormCustomCatalogDropdown />
-        {policyData.catalogQuery.title && <ProvisioningFormCustomCatalogTitle />}
-        {policyData.catalogQuery.contentFilter && <ProvisioningFormCustomCatalogTextArea />}
-        {(policyData.catalogQuery.includeExecEd2UCourses !== undefined)
+  return (
+    <article className="mt-4.5">
+      <ProvisioningFormDefineCustomCatalogHeader index={index} />
+      <ProvisioningFormCustomCatalogDropdown />
+      {policyData.catalogQuery.title && <ProvisioningFormCustomCatalogTitle />}
+      {policyData.catalogQuery.contentFilter && <ProvisioningFormCustomCatalogTextArea />}
+      {(policyData.catalogQuery.includeExecEd2UCourses !== undefined)
         && <ProvisioningFormCustomCatalogExecEdBoolean />}
-      </article>
-    );
-  }
-  return null;
+    </article>
+  );
 };
 
 ProvisioningFormCustomCatalog.propTypes = indexOnlyPropType;
